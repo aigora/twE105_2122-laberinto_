30294cf305d3e86d4bf4b21367f06d4c1ddf4782
Mario <m.csirvent@alumnos.upm.es>
2022-05-18 17:49:15 +0200

    Codigo de prueba eliminado

--- main.c
+++ main.c
@@ -71,46 +71,13 @@ int main()
         }
     }
 
+    for(i=0; i<laberinto.largo; i++)
+    {
+        caracter = laberinto.casillas[i];
 
+        if(caracter == '1')
 
-
-
-    char laberintoPrueba[11][11] = {
-    "XXXXXXXXXX",
-    "XXXXXXXXXX",
-    "XXX    XXX",
-    "XXX XX XXX",
-    "XXXoXX XXX",
-    "XX  XX  XX",
-    "XX XXXX  E",
-    "XX    XXXX",
-    "XXXXX    X",
-    "XXXXXXXXXX"};
-
-    imprime(laberintoPrueba, 10, 10);
-
-
-
-
+    }
 
     return 0;
-}
-
-void imprime(char matriz[11][11], int nFil, int nCol)
-{
-    int i, j;
-    for(i=0; i<=nFil; i++)
-    {
-        for(j=0; j<=nCol; j++)
-            printf("%c", matriz[i][j]);
-        printf("\n");
-    }
-}
-
-void movimiento(char tecla)
-{
-    do
-    {
-
-    } while(x != xe && y != ye);
 }
