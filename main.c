c1bd60d35c9f36e5dcbc84dc868e3b0e7772aadb
Diego Fernandes <d.falves@alumnos.upm.es>
2022-05-20 18:32:33 +0200

    Implementación de Funciones: RellenarCasillas y LeerArchivo

--- main.c
+++ main.c
@@ -106,6 +106,83 @@ int main()
     return 0;
 }
 
+int RellenarCasillas(const char* archivo, Laberinto* laberinto) ///Funcion que rellena las casillas del laberinto a partir de los valores leidos
+{
+    int contador = 0;
+
+    FILE* pf = fopen(archivo, "r"); //Abre el archivo introducido por teclado
+
+    if(pf != NULL)
+    {
+        char valor;
+
+        while(fscanf(pf, "%c", &valor) != EOF) //Lee caracter a caracter el archivo
+        {
+            if(valor != '\n' && valor != ' ') //Si no es un salto de línea o un espacio, entonces es un caracter
+            {
+                laberinto->casillas[contador] = valor; //Almacena el valor leido del archivo en la casilla correspondiente de la estructura Laberinto
+                contador++;
+            }
+
+        }
+
+        return 1; //Lectura correcta
+    }
+
+
+    return 0; //Error al abrir el archivo
+}
+
+int LeerArchivo(const char* archivo, Laberinto* laberinto) ///Funcion para leer el contenido del archivo
+{
+    int primeraLinea = 0;
+    int filas = 1;
+    int columnas = 1;
+
+    FILE* pf = NULL;
+    pf = fopen(archivo, "r");
+
+    if(pf != NULL)
+    {
+        char valor;
+
+
+        while(fscanf(pf, "%c", &valor) != EOF) //Lee caracter a caracter y lo guarda en "valor"
+        {
+            if(valor == '\n') //Cada nueva linea, se incrementa el numero de filas
+            {
+                filas++;
+                primeraLinea = 1;
+            }
+
+            else if(valor == ' ' && primeraLinea == 0) //Cada espacio, se incrementa el numero de columnas
+            {
+                columnas++;
+            }
+        }
+
+        //Parametros del laberinto
+
+        laberinto->largo = filas;
+        laberinto->ancho = columnas;
+        laberinto->dimensiones = filas * columnas;
+
+
+        laberinto->casillas = (char*)malloc(laberinto->dimensiones); //Reserva memoria para la variable casillas
+
+        fclose(pf);
+
+        return RellenarCasillas(archivo, laberinto);
+
+        //Si el archivo se ha leido correctamente, RellenarCasillas = 1 y al llamar a la funcion el contenido de laberinto queda modificado
+    }
+
+    //Error, no se pudo abrir el fichero
+
+    return 0;
+}
+
+
 void DibujarLaberinto(Laberinto laberinto) ///Funcion para imprimir por pantalla el laberinto
 {
     int i = 0;
